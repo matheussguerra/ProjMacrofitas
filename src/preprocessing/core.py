f0e7e0bc3dc9e9b28ab41ef08277a0952eb82da0
Vitor Yudi Shinohara <vitor.ysh@gmail.com>
2018-10-04 15:20:59 -0300

    Preprocessing added.

---
+++ src/preprocessing/core.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def xlsxToCsv(path):
+	data_xlsx = pd.read_excel(path, index_col=None)
+	data_xlsx.to_csv(path[0:-5] + ".csv", encoding='utf-8', index=False)
+
+
+def main():
+	xlsxToCsv("../data/ListaMacrofitas.xlsx")
+
+	ref_arquivo = open("../data/ListaMacrofitas.csv", 'r')
+	result_arquivo = open("../data/ListaMacrofitasResult.csv", 'w')
+
+	for linha in ref_arquivo.readlines():
+		especie = linha.split()
+		result_arquivo.writelines(especie[0][1:] + " " + especie[1] + "\n")
+		print(especie[0][1:] + " " + especie[1])
+
+	ref_arquivo.close()
+	result_arquivo.close()
+
+
+if __name__ == '__main__':
+	main()
