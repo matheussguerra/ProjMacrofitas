c23546ac85622f3ada558719908a5d206cf4645a
vitorys <vitor.ysh@gmail.com>
2018-10-05 13:40:55 -0300

    floraDoBrasil now generating csv as output. PlantList fixed

--- src/requests/plantList.py
+++ src/requests/plantList.py
@@ -1,8 +1,8 @@
 from BeautifulSoup import BeautifulSoup
 import requests
 
-def plantList():
-    response = requests.get('http://www.theplantlist.org/tpl1.1/search?q=Eclipta megapotamica')
+def getData(searchTerm):
+    response = requests.get('http://www.theplantlist.org/tpl1.1/search?q=' + searchTerm)
     raw_http = response.text
 
     soup = BeautifulSoup(raw_http)
@@ -28,10 +28,4 @@ def getSynonymous(soup):
             'date_supplied' : str(tr('td')[4].text)
         }
         synonymous.append(data)
-    return data
-
-def main():
-    plantList()
-
-if __name__ == '__main__':
-    main()
+    return synonymous
