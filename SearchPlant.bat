6fd2bc6d9dcb16c0551a294627430be3bc359134
Matheus Guerra <guerramatheus2@gmail.com>
2018-12-07 18:31:15 -0200

    Update SearchPlant.bat

--- SearchPlant.bat
+++ SearchPlant.bat
@@ -1,3 +1,4 @@
 cd \
 Python27\python.exe -m pip install \Users\%USERNAME%\Downloads\ProjMacrofitas-master\src\requeriments.txt
 Python27\python.exe \Users\%USERNAME%\Downloads\ProjMacrofitas-master\src\main.py
+Pause
