8d0394333704fff30cde1ef20b2d64bcc16f6b14
Matheus Guerra <guerramatheus2@gmail.com>
2018-12-07 19:59:41 -0200

    Update PlantSearch.bat
    adding feedback to user

--- PlantSearch.bat
+++ PlantSearch.bat
@@ -1,7 +1,11 @@
+ECHO OFF
+ECHO *** Instalando/ Verificando dependencias ***
 cd \
 (dir /s /b *requirements.txt) > temp.txt
 SET /p VAR=<temp.txt
 Python27\python.exe -m pip install -r %VAR%
+
+ECHO *** Executando PlantSearch ***
 
 (dir /s /b *PlantSearch.py) > temp.txt
 SET /p VAR=<temp.txt
