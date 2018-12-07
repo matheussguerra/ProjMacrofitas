b4510d85040b0e0d73da92ee810d3d014092c101
Matheus Guerra <guerramatheus2@gmail.com>
2018-12-07 17:18:31 -0200

    Exe para Win

---
+++ SearchPlant.bat
@@ -0,0 +1,3 @@
+cd \
+Python27\python.exe -m pip install \Users\%USERNAME%\Downloads\ProjMacrofitas-master\src\requeriments.txt
+Python27\python.exe \Users\%USERNAME%\Downloads\ProjMacrofitas-master\src\main.py
